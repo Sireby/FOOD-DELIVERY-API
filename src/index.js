0432b0c394191076d98843c45b76542def25a6c8
hikmahDikko <dhywis31@gmail.com>
2022-10-02 18:11:19 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const fs = require("fs");
+const morgan = require("morgan");
+const path = require("path");
+dotenv.config({path : "./config.env"});
+const authRoutes = require("./routes/auth-route");
+const cookieParser = require("cookie-parser");
+
+app.use(express.json());
+
+app.use(cookieParser());
+
+var accessLogStream = fs.createWriteStream(path.join("utils", 'access.log'), {
+    flags : 'a'
+});
+
+app.use(morgan('dev', { stream : accessLogStream }));
+
+app.use("/api/v1/auths", authRoutes);
+
+module.exports = app;
