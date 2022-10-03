09f8ea76f891381eeff49a25c8611f3a00a522e7
MPT-Emeka <ifeanyie31@gmail.com>
2022-10-03 14:08:23 +0100

    product update

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,35 +1,43 @@
 const Product = require("../models/productModel");
-// const Auth = require('./auth')
-// const user = require('./user-model')
+const User = require('../models/user-model')
 const productErrorHandler = require("../helpers/productErrorHandler");
 
-// const productCount = require("./productCounter")
+//const productCount = require("./productCounter")
 
-exports.createProduct = async (request, response) => {
-  const requestBody = request.body;
-  //   if(requestBody.uniqueProductId === Product.uniqueProductId){
-  //     Product.productCount += 1
-  //   }
 
-  let findProduct = await Product.findOne({
-    uniqueProductId: requestBody.uniqueProductId,
-  });
+// let productCounter = Product.aggregate([
+//   {
+//     $group: {
+//       _id: "$productName",
+//       count: { $sum: 1}
+//     }
+//   }
+// ])
+
+// productCounter.length;
+
+
+
+exports.createProduct = async (request, response) => {  
   try {
-    if (findProduct) {
-      findProduct.productCount += 1; // findProduct = new productCount.createdProduct()
-      await findProduct.save();
-      return response.status(200).send({
-        status: false, // check
-        message: "increase product count",
-      });
-    } else {
-      const product = new Product(requestBody);
-      await product.save();
+    const user = await User.findById(request.body.vendor);
+    const productCt = await Product.findOne({ productName: request.body.productName }); 
+    if (user && productCt) {  
+      productCt.productCounter += 1;
+      await productCt.save();
       return response.status(201).send({
-        status: true,
-        message: "product has been uploaded successfully",
-        newProduct: product,
-      });
+        status: true, // check
+        message: "product count has been increased successfully",
+        productCtToReturn: productCt 
+      })
+    } else if (user && !productCt) {
+        const product = new Product(request.body)
+        await product.save();
+        return response.status(201).send({
+                status: true, // check
+                message: "product has been uploaded successfully",
+                productToReturn: product,
+        })
     }
   } catch (error) {
     const err = productErrorHandler(error);
@@ -39,7 +47,8 @@ exports.createProduct = async (request, response) => {
 
 exports.updateProduct = async (request, response) => {
   try {
-    const findProduct = await Product.findOne(request.body.uniqueProductId);
+    const findProduct = await Product.findById(request.params.id);
+    console.log(findProduct)
     if (findProduct) {
       findProduct.price = request.body.price;
       findProduct.description = request.body.description;
@@ -64,7 +73,6 @@ exports.updateProduct = async (request, response) => {
 };
 
 exports.getProduct = async (request, response) => {
-  if (request.query.user == "true") {
     try {
       const id = request.params.id;
       const findOneProduct = await Product.findById(id);
@@ -81,53 +89,60 @@ exports.getProduct = async (request, response) => {
         });
       }
     } catch (err) {
-      if (err.path === "_id") {
         return response.status(401).send({
           status: false,
           message: "Invalid ID",
         });
-      } else {
-        return response.status(500).send({
-          status: false,
-          message: "Server Error",
-        });
-      }
     }
-  } else {
-    response.status(401).send({ status: false, message: "create an account" });
-  }
-};
+  } 
+  
 
 exports.getProductListing = async (request, response) => {
-  if (request.query.user == "true") {
     try {
+      const productCt = await Product.find({
+        productName: request.body.productName,
+      });
+
+      if(productCt) {
+       productCt.productCounter += 1;
+      }
+
+
       const findAllProduct = await Product.find();
       return response.status(200).send({
         status: true,
         message: "Product Listing",
         productListing: findAllProduct,
+        counts: findAllProduct.length,
+        productCounter: findAllProduct.productCounter
       });
     } catch (error) {
       const err = productErrorHandler(error);
       return response.status(400).json({ err });
     }
-  } else {
-    response.status(401).send({ status: false, message: "create an account" });
   }
-};
+
 
 exports.deleteProduct = async (request, response) => {
   try {
     const { id } = request.query;
-    const findProduct = await Product.findByIdAndDelete(id);
-    if (findProduct) {
-      findProduct.productCount -= 1; // findProduct = new productCount.deletedProduct()
+    const findProduct = await Product.findById(id)
+    if (findProduct.productCounter > 1) {
+      findProduct.productCounter -= 1;
+      await findProduct.save();
+      return response.status(200).send({
+        status: true,
+        message: `Single Product ${findProduct.productName} deleted successfully`,
+        deletedProduct: findProduct,
+      });
+    }else if (findProduct.productCounter === 1){
+      findProduct.deleteOne();
       return response.status(200).send({
         status: true,
         message: "Product deleted successfully",
         deletedProduct: findProduct,
-      });
-    } else {
+      })
+    } else if (!findProduct) {
       return response.status(404).send({
         status: false,
         message: "Product not found",
@@ -138,3 +153,4 @@ exports.deleteProduct = async (request, response) => {
     return response.status(400).json({ err });
   }
 };
+
--- src/routes/productRoute.js
+++ src/routes/productRoute.js
@@ -1,19 +1,20 @@
 const express = require("express");
 const productController = require("../controllers/productController");
+const { auth, checkUser } = require("../middleware/authMiddleware");
 
 const productRouter = express.Router();
 
-const { createProduct, updateProduct, getProductListing, deleteProduct } =
+const { createProduct, updateProduct, getProduct, getProductListing, deleteProduct } =
   productController;
 
 productRouter
   .route("/product")
-  .post(createProduct)
-  .put(updateProduct)
-  .get(getProductListing)
-  .delete(deleteProduct);
+  .post(auth, checkUser("vendor"), createProduct)
+  // .put(auth, checkUser("vendor"), updateProduct)
+  .get(auth, getProductListing)
+  .delete(auth, checkUser("vendor", "admin"), deleteProduct);
 
-const { getProduct } = productController;
-productRouter.route("/product/:id").get(getProduct);
+productRouter.put("/product/:id", auth, checkUser("vendor"), updateProduct);
+productRouter.get("/product/:id", auth, getProduct);
 
 module.exports = productRouter;
